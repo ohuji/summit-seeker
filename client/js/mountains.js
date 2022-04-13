e39bfa7d4bf43bdd996a38d677c46b8ad9a6494b
ohuji <juhsalomaki@gmail.com>
2022-04-13 16:15:18 +0300

    mountains files, changed home.css to styles.css, added picture

---
+++ client/js/mountains.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const url = "http://localhost:3030";
+
+const getMountains = async () => {
+    try {
+      const res = await fetch(`${url}/mountains`);
+  
+      return await res.json();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+const renderMountains = async () => {
+    let latestPosts = await getMountains();
+    let html = "";
+  
+    latestPosts.forEach(mountain => {
+      let segment = `<div class="mountain-card">
+        <img src="../media/${mountain.ID}.jpg"
+        alt="picture of a mountain"></img>
+        <h2>${mountain.Name}</h2>
+        <p>${mountain.Difficulty}</p>
+        <p>${mountain.Height}</p>
+        <p>${mountain.Location}</p>
+      </div`;
+  
+      html += segment;
+    });
+  
+    recentList.innerHTML = html;
+  };
