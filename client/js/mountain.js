55a91552b01e960a687185fe4c705d247c31cd10
Remy Silanto <remy.silanto99@gmail.com>
2022-05-05 21:53:51 +0300

    Halfway done developing post creation, making sql query for posts only of one mountain and printing information of current mountain

--- client/js/mountain.js
+++ client/js/mountain.js
@@ -11,7 +11,7 @@ const getCurrentMountain = async () => {
 
   // Mountain ID is got from localStorage and placed to variable
   const mountainID = localStorage.getItem('mID');
-  console.log(mountainID)
+  console.log('mountain id: ', mountainID);
   try {
     const fetchOptions =  {
       headers:  {
@@ -28,17 +28,146 @@ const getCurrentMountain = async () => {
 };
 
 
-const renderCurrentMountain = async () => {
+const renderCurrentMountainImg = async () => {
   let currentMountain = await getCurrentMountain();
   let html = '';
 
   let imageElement = `<img src="./media/${currentMountain.Name}.jpg" id="mountain-image">`;
-
   html += imageElement;
   mountainImgContainer.innerHTML = html;
 };
 
+const renderCurrentMountainInfo = async () => {
+  let currentMountain = await getCurrentMountain();
+  let html = '';
 
+  let infoElement = `<li id="latestPost">
+    <h2 id="mountain-name">${currentMountain.Name}</h2>
+    <h2 id="mountain-difficulty">${currentMountain.Difficulty}</h2>
+    <h2 id="mountain-height">${currentMountain.Height}</h2>
+    <h2 id="mountain-location">${currentMountain.Location}</h2>
+  </li>`;
+  html += infoElement;
+
+  mountainInfoContainer.innerHtml = html;
+}
+
+
+const popularList = document.querySelector('#postList');
+const recentList = document.querySelector('#postList');
+
+const getMostPopular = async () => {
+  try {
+    const res = await fetch(`${url}/popular`);
+
+    return await res.json();
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const getLatest = async () => {
+  try {
+    const res = await fetch(`${url}/latest`);
+
+    return await res.json();
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const renderPopularPosts = async () => {
+  let popularPosts = await getMostPopular();
+  let html = "";
+
+  /*
+  Creates shown element containing little of post data
+  Creates also bigger hidden element inside the shown element which contains much more data
+ */
+  let header = `<h2>Most popular journeys</h2>`;
+  html += header;
+
+  popularPosts.forEach(userPost => {
+    let segment = `<li id="popularPost">
+      <div id="post-figure">
+        <img src="../../server/uploads/${userPost.Imagename}" id="post-image">
+        <div id="post-titleContainer">
+          <h4 id="post-title">${userPost.Title}</h4>
+          <h4 id="post-username">${userPost.Username}</h4>
+        </div>
+      </div> 
+      <article id="post-article">          
+        <p id="post-equipment">${userPost.Equipment}</p>
+        <p id="post-description">${userPost.Description}</p>
+        <div id="post-likes-and-dislikes">
+          <h4 id="post-likes">Likes: ${userPost.Likes}</h4>
+          <h4 id="post-dislikes">Dislikes: ${userPost.Dislikes}</h4>
+        </div>
+      </article>
+    </li>`;
+
+    html += segment;
+  });
+
+  popularList.innerHTML = html;
+
+  /*  // Goes through 10 posts and gives them style
+    for (let i = 0; i < 10; i++) {
+
+      // Sets style to shown posts
+      let currentPost = document.getElementById("popularPost" + i);
+      Object.assign(currentPost.style, postStyle);
+    }*/
+};
+
+const renderLatestPosts = async () => {
+  let latestPosts = await getLatest();
+  let html = "";
+
+  /*
+  Creates shown element containing little of post data
+  Creates also bigger hidden element inside the shown element which contains much more data
+   */
+  let header = `<h2>Most recent journeys</h2>`;
+  html += header;
+
+  latestPosts.forEach(userPost => {
+    let segment = `<li id="latestPost">
+      <div id="post-figure">
+        <img src="../../server/uploads/${userPost.Imagename}" id="post-image">
+        <div id="post-titleContainer">
+          <h4 id="post-title">${userPost.Title}</h4>
+          <h4 id="post-username">${userPost.Username}</h4>
+        </div>
+      </div> 
+      <article id="post-article">          
+        <p id="post-equipment">${userPost.Equipment}</p>
+        <p id="post-description">${userPost.Description}</p>
+        <div id="post-likes-and-dislikes">
+          <h4 id="post-likes">Likes: ${userPost.Likes}</h4>
+          <h4 id="post-dislikes">Dislikes: ${userPost.Dislikes}</h4>
+        </div>
+      </article>
+    </li>`;
+
+    html += segment;
+  });
+
+  recentList.innerHTML = html;
+};
+
+renderLatestPosts();
+
+
+// Handler for most recent/most popular posts slider.
+let checkbox = document.querySelector('input[type="checkbox"]');
+checkbox.addEventListener('change', function () {
+  if (checkbox.checked) {
+    renderLatestPosts();
+  } else {
+    renderPopularPosts();
+  }
+});
 
 
 
@@ -66,4 +195,5 @@ postForm.addEventListener('submit', async (event) => {
   location.href = 'mountain.html';
 });
 
-renderCurrentMountain();
+renderCurrentMountainImg();
+renderCurrentMountainInfo();
--- server/models/postModel.js
+++ server/models/postModel.js
@@ -24,6 +24,41 @@ const createPost = async (post, res) => {
   }
 }
 
+
+const getPopularJourneysByMountain = async (res) => {
+  try {
+    const [rows] = await promisePool.query(`SELECT Userposts.ID,
+        Userposts.MountainID,
+        Userposts.UserID,
+        Userposts.Title,
+        Userposts.Description,
+        Userposts.Equipment,
+        Userposts.Imagename,
+        Userposts.Postedtime,
+        Userposts.Likes,
+        Userposts.Dislikes,
+        Users.ID,
+        Users.Username,
+        Users.Role
+        FROM Userposts
+        INNER JOIN Users
+        ON Userposts.UserID = Users.ID
+        
+        INNER JOIN Mountains
+        ON Userposts.MountainID = ?
+        
+        ORDER BY Userposts.Likes
+        DESC LIMIT 10`);
+
+    return rows;
+  } catch (error) {
+    console.error("error", error.message);
+
+    res.status(500).json({ message: "Something went wrong with popular journeys" });
+    return;
+  }
+};
+
 module.exports = {
   createPost,
 };
